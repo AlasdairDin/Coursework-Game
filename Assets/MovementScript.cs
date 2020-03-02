9cee56b5e16aa2e850f548460daaa3b6a6621c70
AlasdairD <adinnett@gmail.com>
2020-03-02 21:44:21 +0000

    First Upload

---
+++ Assets/MovementScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float sprintSpeed = 12f;
+    public float walkSpeed = 8.0f;
+    public float jumpSpeed = 10.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= walkSpeed;
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        characterController.Move(moveDirection * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            walkSpeed = sprintSpeed;
+        }
+        else
+
+        {
+            walkSpeed = walkSpeed;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            walkSpeed = 4f;
+        }
+    }
+}
---
+++ Assets/mouselook.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouselook : MonoBehaviour
+{
+    public float mouseSens = 100f; //Defines the mouse sensitivity (Could Make Varible?)
+    public Transform playerModel;
+    float xRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; //Stops Cursor From being used
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime; //Mouse Tracking For X axis and Y axis movement
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+
+        xRotation -= mouseY; //y-axis lookscript
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        
+        playerModel.Rotate(Vector3.up * mouseX); //x-axis LookScript
+    }
+}
+    //Helpful Links 
+    // https://www.youtube.com/watch?v=_QajrabyTJc
+    // https://docs.unity3d.com/Manual/CreatingAndUsingScripts.html
+    // https://docs.unity3d.com/Manual/UnderstandingVectorArithmetic.html
+    // https://docs.unity3d.com/Manual/EventFunctions.html
